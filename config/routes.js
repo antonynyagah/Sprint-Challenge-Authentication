79ac70cedbd901e1480eebe7c3cf44c4a3ceb0e3
Antony Nyagah <anttonynyagah@Antonys-MacBook-Pro-2.local>
2019-06-30 22:39:27 -0500

    config commit

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const tokenService = require("../auth/token-service.js");
 
 const { authenticate } = require('../auth/authenticate');
+
+const Users = require("./routes-model");
+const tokenService = require("../auth/token-service");
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +15,39 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      const token = tokenService.generateToken(user);
+      res.status(201).json({ saved, message: `registered, ${token}` });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token,
+          roles: token.roles
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
